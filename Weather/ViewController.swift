2fe650a59f2f1ea3a93ad103c92b05c497d93e0c
Kirill Tarasko <tarasko.kirill@icloud.com>
2025-01-05 22:23:03 +0200

    Add weather api, parsed json

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -1,21 +1,39 @@
-//
-//  ViewController.swift
-//  Weather
-//
-//  Created by Kirill Tarasko on 05.01.2025.
-//
+    //
+    //  ViewController.swift
+    //  Weather
+    //
+    //  Created by Kirill Tarasko on 05.01.2025.
+    //
 
-import UIKit
+    import UIKit
+
 
 class ViewController: UIViewController {
-    @IBOutlet var weatherLabel: UILabel!
-    @IBOutlet var getWeather: UIButton!
-
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var getWeatherButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getWeatherButton.addTarget(self, action: #selector(getWeather), for: .touchUpInside)
     }
-
-
-}
+    
+    @objc func getWeather() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=54.70&longitude=20.51&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(Weather.self, from: data) {
+                DispatchQueue.main.async {
+                    self.weatherLabel.text = "Kaliningrad temperature is \(weather.current.temperature2M)"
+                }
+                
+            } else {
+                print("Error")
+            }
+        }
+            
+            task.resume()
+        }
+    }
+    
 
